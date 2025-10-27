8bb1a0429ce56514f3ad68e4bbed3ca731713d94
nguyenquangvu3003 <vunguyen3303@gmail.com>
2025-10-27 11:02:33 +0700

    first commit

---
+++ js/sum.js
@@ -0,0 +1,18 @@
+// cach dung vong lap
+function sumarray(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers [i]
+    }
+    return sum
+}
+// dung reduce nang cao
+function sumarrayreduce(numbers) {
+    return numbers.reduce((total, num) => total + num, 0)
+}
+//test thu
+const arr = [5, 10, 15, 20]
+console.log('sum:', sumarray(arr));
+console.log('sum(reduce):', sumarrayreduce(arr));
+
+
